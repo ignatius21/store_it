60c9c54f324902d0b69b20caef39b9fa7302547d
ignatius21 <ignaciojosealberto@hotmail.com>
2024-12-19 05:26:13 -0300

    Agregar funcionalidad para renombrar archivos en el menú desplegable de acciones; implementar lógica para manejar acciones de compartir y eliminar

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -27,6 +27,8 @@ import { constructDownloadUrl } from "@/lib/utils";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { set } from "zod";
+import { renameFile } from "@/lib/actions/file.actions";
+import { usePathname } from "next/navigation";
 
 const ActionDropdown = ({ file }: { file: Models.Document }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -34,6 +36,8 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
   const [action, setAction] = useState<ActionType | null>(null);
   const [name, setName] = useState(file.name);
   const [isLoading, setIsLoading] = useState(false);
+
+  const path = usePathname();
 
   const closeAllModal = () => {
     setIsModalOpen(false);
@@ -43,7 +47,21 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
     // setEmails([])
   };
 
-  const handleAction = async () => {};
+  const handleAction = async () => {
+    if(!action) return;
+    setIsLoading(true);
+    let success = false;
+
+    const actions = {
+      rename: () => 
+        renameFile({fileId: file.$id, name,extension:file.extension, path}),
+        share: () => console.log("share"),
+        delete: () => console.log("delete"),
+    };
+    success = await actions[action.value as keyof typeof actions]();
+    if(success) closeAllModal();
+    setIsLoading(false);
+  };
 
   const renderDialogContent = () => {
     if (!action) return null;
