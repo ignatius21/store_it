0b54c01b6463d45ebf183fcec63b75cd71110546
ignatius21 <ignaciojosealberto@hotmail.com>
2024-12-19 20:24:52 -0300

    Agregar funcionalidad para compartir archivos; implementar componente ShareInput y lógica para actualizar usuarios compartidos

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -5,7 +5,6 @@ import React, { useState } from "react";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -26,10 +25,9 @@ import Link from "next/link";
 import { constructDownloadUrl } from "@/lib/utils";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { set } from "zod";
-import { renameFile } from "@/lib/actions/file.actions";
+import { renameFile, updateFileUsers } from "@/lib/actions/file.actions";
 import { usePathname } from "next/navigation";
-import { FileDetails } from "./ActionsModalContent";
+import { FileDetails, ShareInput } from "./ActionsModalContent";
 
 const ActionDropdown = ({ file }: { file: Models.Document }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -37,6 +35,7 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
   const [action, setAction] = useState<ActionType | null>(null);
   const [name, setName] = useState(file.name);
   const [isLoading, setIsLoading] = useState(false);
+  const [emails, setEmails] = useState<string[]>([])
 
   const path = usePathname();
 
@@ -56,13 +55,22 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
     const actions = {
       rename: () => 
         renameFile({fileId: file.$id, name,extension:file.extension, path}),
-        share: () => console.log("share"),
+        share: () => updateFileUsers({fileId: file.$id, emails, path}),
         delete: () => console.log("delete"),
     };
     success = await actions[action.value as keyof typeof actions]();
     if(success) closeAllModal();
     setIsLoading(false);
   };
+
+  const handleRemoveUser = async (email: string) => {
+    const updatedEmails = emails.filter((e) => e !== email);
+
+    const success = await updateFileUsers({fileId: file.$id, emails: updatedEmails, path});
+
+    if(success) setEmails(updatedEmails);
+    closeAllModal();
+  }
 
   const renderDialogContent = () => {
     if (!action) return null;
@@ -81,6 +89,7 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
             />
           )}
           {value === 'details' && <FileDetails file={file} />}
+          {value === 'share' && <ShareInput file={file} onInputChange={setEmails} onRemove={handleRemoveUser}/>}
         </DialogHeader>
         {["rename", "delete", "share"].includes(value) && (
           <DialogFooter className="flex flex-col gap-3 md:flex-row">
@@ -127,7 +136,7 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
               onClick={() => {
                 setAction(actionItem);
                 if (
-                  ["rename", "share,delete", "details"].includes(
+                  ["rename", 'share','delete', "details"].includes(
                     actionItem.value
                   )
                 ) {
--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -97,3 +97,23 @@ export const renameFile = async ({fileId, name, path, extension}:RenameFileProps
         
     }
 }
+
+export const updateFileUsers = async ({fileId, emails, path}:UpdateFileUsersProps) => {
+    const {databases} = await createAdminClient()
+    try {
+        const updatedFile = await databases.updateDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.filesCollectionId,
+            fileId,
+            {
+                users: emails,
+            }
+        );
+        revalidatePath(path)
+        return parseStringify(updatedFile)
+
+    } catch (error) {
+        handleError(error, "Failed to rename file")
+        
+    }
+}
