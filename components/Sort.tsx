faead42a65a05110211546e7f7c8614ff40a5dd2
ignatius21 <ignaciojosealberto@hotmail.com>
2024-12-26 05:18:34 -0300

    Agregar componente Select para mejorar la funcionalidad de ordenamiento; actualizar lógica en Sort y agregar dependencia en package.json

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,9 +1,41 @@
+'use client'
+
 import React from 'react'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { usePathname, useRouter } from 'next/navigation';
+import { sortTypes } from '@/constants';
+
 
 const Sort = () => {
+
+  const router = useRouter()
+
+  const path = usePathname()
+
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`)
+  }
   return (
-    <div>Sort</div>
-  )
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className='sort-select-content'>
+        {sortTypes.map((item, index) => (
+          <SelectItem key={index} value={item.value} className='shad-select-item'>
+            {item.label}
+          </SelectItem>
+        )
+        )}
+      </SelectContent>
+    </Select>
+  );
 }
 
 export default Sort
